2340c228c3213f366c21c3d40721ba73bfc2a10b
PedroJSBezerra <pjsoares46@gmail.com>
2021-09-25 00:57:51 -0300

    add sass

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import svelte from 'rollup-plugin-svelte';
+import autoPreprocess from 'svelte-preprocess'
+
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -41,7 +44,8 @@ export default {
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
-			}
+			},
+			preprocess: autoPreprocess()
 		}),
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
